fc72a75dc974b875e9fd202d46f87f4b1b2a07cf
Nicholas Anderson <nicholas@nickelos.org>
2020-09-16 21:06:12 -0400

    finished? Gui integration, now Hammond just needs to use it

--- src/Project1.java
+++ src/Project1.java
@@ -1,57 +1,71 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
-
-public class Project1 extends Synth{
-  public static void main(String[] args){
+public class Project1 extends Synth {
+  public static void main(String[] args) {
     Synth.run(new Project1(), args);
   }
 
-  public void setup(){
+  public void setup() {
     // Implement me:
 
-		// Build a MidiModule
-		MidiModule midimod = new MidiModule(getMidi());
-        modules.add(midimod);
+    // Build a MidiModule
+    MidiModule midimod = new MidiModule(getMidi());
+    modules.add(midimod);
 
-		// Build a MidiGate
-		MidiGate gate = new MidiGate(midimod);
-        modules.add(gate);
+    // Build a MidiGate
+    MidiGate gate = new MidiGate(midimod);
+    modules.add(gate);
 
-        // Create the window
+    // Create the window
     JFrame frame = new JFrame();
     Box outer = new Box(BoxLayout.X_AXIS);
     frame.setContentPane(outer);
-    //layout for osciloscope output
+    // layout for osciloscope output
     Box outputBox = new Box(BoxLayout.Y_AXIS);
     outputBox.setBorder(BorderFactory.createTitledBorder("Output"));
     outer.add(outputBox);
     Dial dial = new Dial(1.0);
     outputBox.add(dial.getLabelledDial("Gain"));
 
-    // Build a Hammond Organ with dials and options
-    Hammond ham = new Hammond();
-    ham.setFrequencyMod(midimod);
-    modules.add(ham);
-
     // Build an Output
     Box hammondBox = new Box(BoxLayout.Y_AXIS);
     hammondBox.setBorder(BorderFactory.createTitledBorder("Hammond"));
     outer.add(hammondBox);
 
-    //build gui for Hammond
-    for(int i= 1;i<=8;i++){
-      hammondBox.add((new Dial(i)).getLabelledDial(Integer.toString(i)));
+    // build gui for Hammond
+    Dial[] hDials = new Dial[9];
+    for (int i = 1; i <= 9; i++) {
+      Dial tDial = new Dial(i);
+      hammondBox.add(tDial.getLabelledDial(Integer.toString(i)));
+      hDials[i - 1] = tDial;
     }
+
+    // Build a Hammond Organ with dials and options
+    Hammond ham = new Hammond(hDials);
+    ham.setFrequencyMod(midimod);
+    modules.add(ham);
+
     String[] names = new String[Hammond.PRESETS.length];
-    for(int i=0;i<Hammond.PRESETS.length;i++){
+    for (int i = 0; i < Hammond.PRESETS.length; i++) {
       names[i] = (String) Hammond.PRESETS[i][0];
-      //Multiple presets have the same name as others, Do not uncomment for fear
+      // Multiple presets have the same name as others, Do not uncomment for fear
       // of immense confusion!!!
-      //trim the leading numbers from the names
-      //names[i] = names[i].split(" ")[1];
+      // trim the leading numbers from the names
+      // names[i] = names[i].split(" ")[1];
     }
     JComboBox<String> presets = new JComboBox<String>(names);
+    presets.addItemListener((ItemListener) new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent itemEvent) {
+        @SuppressWarnings("unchecked")
+        JComboBox<String> tBox = (JComboBox<String>)itemEvent.getSource();
+        ham.setPreset(tBox.getSelectedItem().toString());
+      }
+    });
 
     hammondBox.add(presets);
 
@@ -76,7 +90,8 @@ public class Project1 extends Synth{
 		outputBox.add(oscope);
 
     // Set the oscilloscope's output as our audio output.
-    // We could have also set the gain module output as our audio output as well, doesn't matter.
+    // We could have also set the gain module output as our audio output
+    // as well, doesn't matter.
 		setOutput(omodule);
 
 
--- src/README.txt
+++ src/README.txt
@@ -1,4 +1,5 @@
-PROJECT 1: Additive Synthesis Part 1.  You will create a small MONOPHONIC additive Hammond organ.  In Part 2 you'll add some modulation, but not now.  To build the organ you will:
+PROJECT 1: Additive Synthesis Part 1.  You will create a small MONOPHONIC additive Hammond organ.
+			In Part 2 you'll add some modulation, but not now.  To build the organ you will:
 
 	1. Build a module class called Hammond.java
 	2. Copy over your Sine.java file from project0
@@ -7,23 +8,29 @@ PROJECT 1: Additive Synthesis Part 1.  You will create a small MONOPHONIC additi
 
 
 NOTES
-	A Hammond organ has nine TONE WHEELS which produce sine waves and spin at different frequencies relative to the base frequency of your note (that's actually all a lie, it reuses tonewheels to allow for chords etc., but we'll go for it as it's conceptually simple).  Each tonewheel is associated with a DRAW BAR the musician can pull out to specify the amplitude of the tonewheel's sine wave.  The draw bar goes 0...8, where 0 is off and 8 is maximally loud.  Your module should be able to have each of these values set independently or collectively according to one of several presets.  I provide some presets for you.
-	
-	When you play a note, the organ samples its nine tone wheels, multiplies the samples against their drawbar amplitudes, averages them, and returns that.  Thus your hammond organ module will maintain nine modules within it and must call doUpdate() on the Multipliers and the Tone Wheels it is ticked.
-	
+	A Hammond organ has nine TONE WHEELS which produce sine waves and spin at different frequencies relative to the base frequency of your note
+	(that's actually all a lie, it reuses tonewheels to allow for chords etc., but we'll go for it as it's conceptually simple).
+	Each tonewheel is associated with a DRAW BAR the musician can pull out to specify the amplitude of the tonewheel's sine wave.
+	The draw bar goes 0...8, where 0 is off and 8 is maximally loud.
+	Your module should be able to have each of these values set independently or collectively according to one of several presets.
+	I provide some presets for you.
+
+	When you play a note, the organ samples its nine tone wheels, multiplies the samples against their drawbar amplitudes, averages them, and returns that.
+	Thus your hammond organ module will maintain nine modules within it and must call doUpdate() on the Multipliers and the Tone Wheels it is ticked.
+
 	Note that the nine drawbars are not *entirely* in frequency order.  That's traditional.
-	
+
 	When averaging the sine waves, keep in mind that they are centred around 0.5 as their zero-point.  You'll have to do a tiny bit of math.
-	
-	
-	
+
+
+
 RUNNING THE DEMO
 
 The demo is jar/project1.jar
 
 1. If you are running on a Mac, you must put the library coremidi4j-1.1.jar in your /Library/Java/Extensions/  directory.
 
-2. You can run the code as 
+2. You can run the code as
 
 	java -jar jar/project1.jar
         ... to get the list of MIDI devices and audio devices.
