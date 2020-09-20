57e8ea8089c4fbc6b8681f28003809b26e1f18cd
Nicholas Anderson <nicholas@nickelos.org>
2020-09-20 17:07:03 -0400

    Merged with new project code. (Old project1.java is now Project1.1.java)

--- src/Project1.java
+++ src/Project1.java
@@ -1,112 +1,53 @@
 import javax.swing.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.*;
 
-public class Project1 extends Synth {
-  public static void main(String[] args) {
-    Synth.run(new Project1(), args);
-  }
 
-  public void setup() {
-    // Implement me:
+public class Project1 extends Synth
+	{
+    public static void main(String[] args) 
+    	{
+    	Synth.run(new Project1(), args);
+    	}
 
-    // Build a MidiModule
-    MidiModule midimod = new MidiModule(getMidi());
-    modules.add(midimod);
+    public void setup() 
+    	{
+    	// Implement me:
+    	
+    	// Build a MidiModule
+    	// Build a MidiGate
+    	// Create the window
+    	
+    	// BEFORE THE ORGAN:
+ 		// Create an Add which takes the MidiMod pitch as input	
+ 		
+    	// Build a Hammond Organ with dials and options.  It now takes the *Add* as its frequency mod
+    	
+    	// THE ENVELOPE:
+    	
+    	// Build an ADSR with dials at least.  It should default to sine waves
+    	// -- If you were really cool you'd add an options to change the wave type
+    	// -- If you were REALLY cool you'd add additional options for random LFOs or Sample and Hold Random LFOs.
+    	// Build an amplifier AAA that takes the Hammond as input, modulated by the ADSR
+ 
+ 		// THE LESLIE (DOPPLER EFFECT):
 
-    // Build a MidiGate
-    MidiGate gate = new MidiGate(midimod);
-    modules.add(gate);
+    	// Build an LFO.  Its rate should be a dial multiplied by 0.001 (use Mul) to slow it down
+    	// First we'll make the Pitch effect:
+    	// Make a Mul whose input is the LFO, multiplied by a "pitch" dial.
+    	// Make another Mul whose input is the previous Mul multiplied by 0.001 so the pitch doesn't go nuts
+    	// Set this final Mul as the adder() for the Add so it affects the organ's pitch
+    	// Next we'll make the volume effect:
+    	// Make yet another Mul, with the LFO as input, and an "Amplitude" dial as multiplier.
+    	// Make another Add, with the Mul as the adder(), and that's it.  No input.  (thus it's 1.0 + Mul)
+    	// Make an amplifier BBB with the first amplifier AAA as input and this new Add as modulation to flutter the overall volume.
 
-    // Create the window
-    JFrame frame = new JFrame();
-    Box outer = new Box(BoxLayout.X_AXIS);
-    frame.setContentPane(outer);
-
-    // Build an Output
-    Box hammondBox = new Box(BoxLayout.Y_AXIS);
-    hammondBox.setBorder(BorderFactory.createTitledBorder("Organ"));
-    outer.add(hammondBox);
-
-    // build gui for Hammond
-    Dial[] hDials = new Dial[9];
-    for (int i = 1; i <= 9; i++) {
-      Dial tDial = new Dial(i);
-      hammondBox.add(tDial.getLabelledDial("Bar " + Integer.toString(i)));
-      hDials[i - 1] = tDial;
+    	// Build an Output
+    	// Build a Gate amplifier.  Its signal now gets fed *amplifier BBB*
+    	// Add a Gain amplifier fed the Gate amplifier
+    	// Add an Oscillocope
+    	// Set the output to the gain amplifier (or oscilloscope)
+    	// Pack and display the window
+        }
+        
     }
-
-    // Build a Hammond Organ with dials and options
-    Hammond ham = new Hammond(hDials);
-    ham.setFrequencyMod(midimod);
-    modules.add(ham);
-
-    String[] names = new String[Hammond.PRESETS.length];
-    for (int i = 0; i < Hammond.PRESETS.length; i++) {
-      names[i] = (String) Hammond.PRESETS[i][0];
-      // Multiple presets have the same name as others, Do not uncomment for fear
-      // of immense confusion!!!
-      // trim the leading numbers from the names
-      // names[i] = names[i].split(" ")[1];
-    }
-    hammondBox.add(new JLabel("Presets"));
-
-    JComboBox<String> presets = new JComboBox<String>(names);
-    presets.addItemListener((ItemListener) new ItemListener() {
-      @Override
-      public void itemStateChanged(ItemEvent itemEvent) {
-        @SuppressWarnings("unchecked")
-        JComboBox<String> tBox = (JComboBox<String>)itemEvent.getSource();
-        ham.setPreset(tBox.getSelectedItem().toString());
-      }
-    });
-
-    hammondBox.add(presets);
-
-		// Build a Gate amplifier
-		Amplifier amp = new Amplifier();
-    amp.setInput(ham);
-    amp.setAmplitudeMod(gate);
-		modules.add(amp);
-
-    // layout for osciloscope output
-    Box outputBox = new Box(BoxLayout.Y_AXIS);
-    outputBox.setBorder(BorderFactory.createTitledBorder("Output"));
-    outer.add(outputBox);
-    Dial dial = new Dial(1.0);
-    outputBox.add(dial.getLabelledDial("Gain"));
-
-    // Add a Gain amplifier
-		Amplifier gain = new Amplifier();
-    gain.setInput(amp);
-    gain.setAmplitudeMod(dial.getModule());
-    modules.add(gain);
-
-    // Add an Oscillocope
-    // Set the output to the gain amplifier (or oscilloscope)
-    Oscilloscope oscope = new Oscilloscope();
-    Oscilloscope.OModule omodule = oscope.getModule();
-    modules.add(omodule);
-    omodule.setAmplitudeModule(gain);
-		outputBox.add(oscope);
-
-    // Set the oscilloscope's output as our audio output.
-    // We could have also set the gain module output as our audio output
-    // as well, doesn't matter.
-		setOutput(omodule);
-
-
-		//on X quit fixes where the program continues running while window is closed
-		frame.addWindowListener(new java.awt.event.WindowAdapter() {
-			@Override
-			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-        System.exit(0);
-			}
-		});
-
-		// Pack and display the window
-    frame.pack();
-    frame.setVisible(true);
-  }
-}
 
